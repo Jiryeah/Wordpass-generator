5ff7880b1fb56aecc70d00e7e09f731bd30a6345
Chidori <business.faissal@outlook.com>
2021-12-10 22:40:28 -0600

    bugs fixed

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -4,70 +4,70 @@ var generateBtn = document.querySelector("#generate");
 
 var generatePassword = function (){
   // Available Characters 
-  var specialCharacters = '!, ", #, $, %, &, (, ), *, +, ",", -, ".", /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, ~,';
-  var numericCharacters = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9';
-  var lowerCaseLetters = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z';
-  var upperCaseLetters = 'A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z';
+  var specialCharacters = '!"#$%&( )*+","-"."/:;<=>?@[\]^_`{|}~,';
+  var numericCharacters = '0123456789';
+  var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var finalPassword = '';
   var availableCombo = '';
   
   // Prompt to inform the user to choose between 8-128 characters
-  var passwordLength = prompt("Please choose a password length between 8 and 128 characters.");
+  var passwordLength = parseInt(prompt("Please choose a password length between 8 and 128 characters."));
 
     if ((passwordLength >= 8) || (passwordLength <= 128)) {
     console.log(passwordLength)
   }
-  // if it doesn't mean this criteria, create an alert to notify the user 
+  // if it doesn't mean this criteriacreate an alert to notify the user 
   else {
     window.alert ("The password must be between 8 and 128 characters. Please try again.")
     return "";
   };
 
   // Create the confirm windows to aid in the decision of the compostition of the password. 
-  var specialCharacters = confirm("Would you like to use any special characters to fortify the password?");
-  var numericCharacters = confirm("Would you like to use numbers to fortify your password?");
-  var lowerCaseLetters = confirm("Would you like to use lowercase letters to fortify your password?");
-  var upperCaseLetters = confirm("Would you like to use any uppercase letters to fortify your password? ");
+  var specialCharactersPrompt = confirm("Would you like to use any special characters to fortify the password?");
+  var numericCharactersPrompt = confirm("Would you like to use numbers to fortify your password?");
+  var lowerCaseLettersPrompt = confirm("Would you like to use lowercase letters to fortify your password?");
+  var upperCaseLettersPrompt = confirm("Would you like to use any uppercase letters to fortify your password? ");
   // End of Confirm Windows
 
   // Create the conditionals to direct the results gathered from the confirm windows.
-  if (specialCharacters) {
+  if (specialCharactersPrompt) {
     availableCombo += specialCharacters;
   }
 
-  if (numericCharacters) {
+  if (numericCharactersPrompt) {
     availableCombo += numericCharacters;
   }
 
-  if (lowerCaseLetters) {
+  if (lowerCaseLettersPrompt) {
     availableCombo += lowerCaseLetters;
   }
 
-  if (upperCaseLetters) {
+  if (upperCaseLettersPrompt) {
     availableCombo += upperCaseLetters;
   }
 
-  if (!specialCharacters && !numericCharacters && !lowerCaseLetters && !upperCaseLetters) {
+  if (availableCombo.length === 0) {
     return window.alert("One criteria HAS to be met!")
   }
-  // End of Conditionals 
 
+  console.log(availableCombo);
+  // End of Conditionals 
+  console.log(typeof passwordLength);
   // Create index to randomize the selection. 
   for (var index = 0; index < passwordLength; index++) {
-    finalPassword += availableCombo[Math.floor(Math.random () * availableCombo.length)];
+    finalPassword += availableCombo.charAt(Math.floor(Math.random () * availableCombo.length));
     console.log(finalPassword);
-    return finalPassword;
   }
+  return finalPassword;
 };
 
-generatePassword();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); // generatePassword() should return a value
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 };
 
 // Add event listener to generate button
